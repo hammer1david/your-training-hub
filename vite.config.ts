d37c995301211474a390285c113e6cef0537e0b0
hammer1david <hammer1.david@gmail.com>
2026-08-16 21:32:15 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   tanstackStart: {
     spa: {
+      enabled: true,
       prerender: {
         outputPath: "/_shell.html",
         crawlLinks: true,
